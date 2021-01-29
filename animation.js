fa26a8d0015d302fe7d657de723b86f57f14b5d8
Jakub Kądziołka <kuba@kadziolka.net>
2021-01-29 23:48:19 +0100

    Ad-hoc DFS approach

--- animation.js
+++ animation.js
@@ -93,13 +93,13 @@ class Graph extends React.Component {
                 const [color, node] = highlight;
                 const {x, y} = getNodePosition(node);
                 elems.push(<circle
-                    key={node+ "-highlight"}
+                    key={node + "-" + color}
                     className="highlight" stroke={color}
                     cx={x} cy={y} r={radius} />);
             } else {
                 const [color, a, b] = highlight;
                 const {x1, y1, x2, y2} = getLinkPosition(a, b);
-                elems.push(<line key={a + '-' + b + '-highlight'}
+                elems.push(<line key={a + '-' + b + '-' + color}
                     x1={x1} y1={y1} x2={x2} y2={y2}
                     className="highlight" stroke={color}
                 />);
@@ -108,6 +108,8 @@ class Graph extends React.Component {
 
         return <svg fill="none" stroke="white" strokeWidth="3" viewBox={"0 0 " + width + " " + height}>
             {elems}
+            <text textAnchor="end" dominantBaseline="hanging" x={width} y="30"
+            stroke="none" fill="white" style={{ fontSize: 30 }}>{this.props.comment}</text>
         </svg>;
     }
 }
@@ -161,28 +163,92 @@ class StaggerredHighlights extends React.Component {
             graph={this.props.graph}
             layout={this.props.layout}
             highlight={highlight}
+            comment={this.props.comment}
         />;
     }
 }
 
-export function Animation(props) {
-    const highlights = [
-        // Weźmy sobie jakiś graf
-        [],
-        // Chcemy wiedzieć, czy między dwoma węzłami istnieje ścieżka. Na przykład,
-        // jeśli weźmiemy 1 i 6,
-        [[['green', 1], ['green', 6]]],
-        // to możemy przejść między nimi, przez 4 i 5
-        [[['green', 1], ['green', 6]],
-         [['blue', 1, 5],
-          ['blue', 5]],
-         [['blue', 5, 4],
-          ['blue', 4]],
-         [['blue', 4, 6]],
-         ],
-    ];
+function animateLayers(start, color, target, targetColor) {
+    let steps = [];
+    let nextQueue = [start];
+    let visited = new Set();
+    visited.add(start);
+    while (nextQueue.length) {
+        let currentQueue = nextQueue;
+        let currentStep = [];
+        nextQueue = [];
+        while (currentQueue.length) {
+            const node = currentQueue.pop();
+            g.forEachLinkedNode(node, (a, L) => {
+                a = a.id;
+                console.log(a);
+                if (!visited.has(a)) {
+                    visited.add(a);
+                    nextQueue.push(a);
+                    currentStep.push([a == target ? targetColor : color, a]);
+                }
 
-    const step = useSteps(highlights.length - 1);
-    const highlight = highlights[step];
-    return <StaggerredHighlights graph={g} layout={layout} baseHighlights={[]} highlights={highlight} />
+                if (!visited.has([node, a]) && !visited.has([a, node])) {
+                    visited.add([node, a]);
+                    currentStep.push([color, node, a]);
+                }
+            });
+        }
+        steps.push(currentStep);
+    }
+    console.log(visited);
+    return steps;
+}
+
+const steps = [
+    // Weźmy sobie jakiś graf
+    ['reset'],
+    // Chcemy wiedzieć, czy między dwoma węzłami istnieje ścieżka. Na przykład,
+    // jeśli weźmiemy 1 i 6,
+    ['add', [['green', 1], ['green', 6]]],
+    // to możemy przejść między nimi, przez 4 i 5
+    ['add',
+     [['blue', 1, 5],
+      ['blue', 5]],
+     [['blue', 5, 4],
+      ['blue', 4]],
+     [['blue', 4, 6]],
+     ],
+    // Lecz jeśli weźmiemy sobie 6 i 9, to nie ma między nimi ścieżki.
+    ['reset', [['red', 6], ['red', 9]]],
+    // Jak możemy odpowiedzieć na to pytanie?
+    ['reset'],
+    // Moglibyśmy zrobić tak, że weźmiemy jeden z końców...
+    ['add', [['green', 1]]],
+    // i puścimy wyszukiwanie, w głąb lub wszerz, nie ma znaczania
+    ['add'].concat(animateLayers(1, 'blue', 6, 'green')),
+    ['setComment', 'O(q(n + k))'],
+];
+
+export function Animation(props) {
+    function applySteps(base, steps) {
+        for (const step of steps) {
+            base = base.concat(step);
+        }
+        return base;
+    }
+
+    const stepTypes = {
+        'reset': (step, [base, current, comment]) =>
+            [[], step, ""],
+        'add': (step, [base, current, comment]) =>
+            [applySteps(base, current), step, comment],
+        'setComment': (step, [base, current, _]) =>
+            [applySteps(base, current), [], step[0]],
+    };
+
+    const step = useSteps(steps.length - 1);
+    let state = [[], [], ''];
+    for (let i = 0; i <= step; i++) {
+        const stepType = stepTypes[steps[i][0]];
+        state = stepType(steps[i].slice(1), state);
+    }
+
+    return <StaggerredHighlights graph={g} layout={layout}
+        baseHighlights={state[0]} highlights={state[1]} comment={state[2]} />
 }
